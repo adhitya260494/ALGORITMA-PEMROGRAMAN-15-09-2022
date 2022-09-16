2f61b282437a441c611ba80681292457257c0a3d
Adhitya Bagus Kusuma <32472200+adhitya260494@users.noreply.github.com>
2022-09-16 14:39:17 +0700

    Create hasil terminal program13.c

---
+++ hasil terminal program13.c
@@ -0,0 +1,7 @@
+linux@linux-Virtual-Machine:~/Documents/algoritma$ gcc program13.c -o program13
+linux@linux-Virtual-Machine:~/Documents/algoritma$ ./program13
+inputkan angka bulat :3
+ini angka Ganjil
+linux@linux-Virtual-Machine:~/Documents/algoritma$ ./program13
+inputkan angka bulat :9000
+ini angka Genap
