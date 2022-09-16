44f1333c09a8ef7942cc9201180d0f8fd7b54a38
Adhitya Bagus Kusuma <32472200+adhitya260494@users.noreply.github.com>
2022-09-16 14:21:05 +0700

    Create hasil terminal program12.c

---
+++ hasil terminal program12.c
@@ -0,0 +1,9 @@
+linux@linux-Virtual-Machine:~/Documents/algoritma$ gcc program12.c -o program12
+linux@linux-Virtual-Machine:~/Documents/algoritma$ ./program12
+3
+5
+angka 1 < angka 2
+linux@linux-Virtual-Machine:~/Documents/algoritma$ ./program12
+5
+3
+angka1 > angka 2
