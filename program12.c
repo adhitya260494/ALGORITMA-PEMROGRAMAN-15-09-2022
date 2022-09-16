d0459d6d5811f3c6d8280d504c2b7db9dc2f60b9
Adhitya Bagus Kusuma <32472200+adhitya260494@users.noreply.github.com>
2022-09-16 14:20:12 +0700

    Create program12.c

---
+++ program12.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int angka1, angka2;
+
+    scanf("%d",&angka1);
+    scanf("%d",&angka2);
+
+    if (angka1 > angka2)
+        printf("angka1 > angka 2\n");
+    else 
+        printf ("angka 1 < angka 2\n");
+
+    return 0;    
+}
