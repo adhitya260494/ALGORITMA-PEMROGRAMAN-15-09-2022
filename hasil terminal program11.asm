ceabe839add4c023e4d2245e7252ce353e25333a
Adhitya Bagus Kusuma <32472200+adhitya260494@users.noreply.github.com>
2022-09-16 13:52:14 +0700

    Create hasil terminal program11.asm

---
+++ hasil terminal program11.asm
@@ -0,0 +1,10 @@
+linux@linux-Virtual-Machine:~/Documents/algoritma$ nasm -f elf64 program11.asm
+linux@linux-Virtual-Machine:~/Documents/algoritma$ ld program11.o -o program11
+linux@linux-Virtual-Machine:~/Documents/algoritma$ ./program11
+5
+3
+angka 1 > angka 2
+linux@linux-Virtual-Machine:~/Documents/algoritma$ ./program11
+3
+5
+angka 1 < angka 2
